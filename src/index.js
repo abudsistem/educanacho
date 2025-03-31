665d378dbdefa2a5e0610623e970a95796da81e7
abudsistem <abudsistem@gmail.com>
2025-03-31 12:32:24 -0500

    deploy 4 cambiando la direccion

--- src/index.js
+++ src/index.js
@@ -38,23 +38,25 @@ const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const comentRoutes = require('./routes/comentRoutes');
 const path = require('path');
+const compression = require('compression')
 
 dbConnect();
 
 const app = express();
+app.use(compression())
 
 app.use(cors());
 app.use(express.json());
 
-// Servir archivos estáticos desde la carpeta `dist` (que está en la carpeta `backend`)
+//Servir archivos estáticos desde la carpeta `dist` (que está en la carpeta `backend`)
 app.use(express.static(path.join(__dirname, '../dist')));
 
-// Rutas de la API
+//Rutas de la API
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/coments', comentRoutes);
 
-// Cualquier otra ruta servirá el `index.html` del frontend (React)
+//Cualquier otra ruta servirá el `index.html` del frontend (React)
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
@@ -64,3 +66,61 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto: ${PORT}`);
 });
+
+
+/*
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const dbConnect = require('./config/dbConnect');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const comentRoutes = require('./routes/comentRoutes');
+const path = require('path');
+const compression = require('compression');
+
+dbConnect();
+
+const app = express();
+app.use(compression());
+
+// Configurar CORS con credenciales
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(express.json());
+
+// Servir archivos estáticos desde `dist`
+app.use(express.static(path.join(__dirname, '../dist'), {
+    etag: true,  // Habilita ETags para optimización de caché
+    lastModified: true
+}));
+
+// Middleware para configurar `Cache-Control` y compatibilidad con bfcache
+app.use((req, res, next) => {
+    if (req.method === 'GET' && req.headers.accept &&req.headers.accept.includes('text/html')) {
+        res.set('Cache-Control', 'public, max-age=3600, must-revalidate'); // Permite bfcache
+    }
+    next();
+});
+
+// Rutas de la API
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/coments', comentRoutes);
+
+// Redirección de todas las rutas al frontend
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor corriendo en el puerto: ${PORT}`);
+});
+
+*/
