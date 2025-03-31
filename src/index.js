ef4075106c10f40e5ab1e8b3a03acb1cf45a45e0
abudsistem <abudsistem@gmail.com>
2025-03-30 20:18:30 -0500

    deploy 4 cambiando la direccion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
@@ -25,4 +26,41 @@ app.listen(PORT, () => {
     console.log(`JSON_WEB_TOKEN_SECRET: ${process.env.JSON_WEB_TOKEN_SECRET}`);
     console.log(`MONGO_URI: ${process.env.MONGO_URI}`);
     console.log('Server is running');
+});
+
+*/
+
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const dbConnect = require('./config/dbConnect');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const comentRoutes = require('./routes/comentRoutes');
+const path = require('path');
+
+dbConnect();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Servir archivos estáticos desde la carpeta `dist` (que está en la carpeta `backend`)
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// Rutas de la API
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/coments', comentRoutes);
+
+// Cualquier otra ruta servirá el `index.html` del frontend (React)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto: ${PORT}`);
 });
